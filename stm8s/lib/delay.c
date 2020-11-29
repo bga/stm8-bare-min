9e157aa40432343fc3e2e780d8571477026f561a
bga <bga.email@gmail.com>
2020-11-29 14:34:33 +0300

    New `delay_*` implementation. sdcc & iar

--- stm8s/lib/delay.c
+++ stm8s/lib/delay.c
@@ -1,3 +1,32 @@
 #include "delay.h"
 
-extern inline void delay_ms(uint32_t ms);
+#if defined(__ICCSTM8__)
+
+  void delay_cycle(uint16_t ticksCount /* X register */) {
+    asm(
+      "bga_delay_cycle_loop: decw X\n"
+      "nop\n"
+      "jrne bga_delay_cycle_loop\n"
+    );
+  }
+#elif defined(__SDCC_stm8)
+
+  void delay_cycle(uint16_t ticksCount /* (0x03, sp) */) {
+    __asm
+      pushw x
+      ldw x, (3, sp)
+      00001$: decw x
+      nop
+      jrne 00001$
+      popw x
+    __endasm;
+  } 
+#else
+	#error Unsupported compiler
+#endif
+
+void delay_ms(uint_fast16_t ms) {
+	while(ms--) {
+		delay_us(1000);
+	}
+}
--- stm8s/lib/delay.h
+++ stm8s/lib/delay.h
@@ -8,10 +8,25 @@
 
 #include <stdint.h>
 
-inline void delay_ms(uint32_t ms) {
-    for (uint32_t i = 0; i < ((F_CPU / 18 / 1000UL) * ms); i++) {
-        __asm__("nop");
-    }
+//# wrong, uint32_t overflow
+// #define delay_usToLoopCyclesCount(x) ((( F_CPU * (xArg) / 1000000UL ) - 0) / 4)
+
+//# ok,  but lets avoid non power of 2 div
+//# #define delay_usToLoopCyclesCount(xArg) ((( F_CPU / 1000UL * (xArg) / 1000UL ) - 0 ) / 4)
+
+#define delay_usToLoopCyclesCount(xArg) ((( F_CPU / 976UL * (xArg) / 1024UL ) - 0 ) / 4 /* ticka */)
+
+#if defined(__SDCC_stm8)
+  #pragma callee_saves delay_cycle
+#endif
+
+
+void delay_cycle(uint16_t ticksCount);
+
+static inline void delay_us(uint_fast16_t us) {
+	delay_cycle(delay_usToLoopCyclesCount(us));
 }
+
+void delay_ms(uint_fast16_t ms);
 
 #endif /* DELAY_H */
